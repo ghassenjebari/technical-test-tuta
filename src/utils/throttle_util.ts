5843e6e5a42923f5ba990019a7ef5c9848d22dbe
ghassenjebari <ghassenjebari1@gmail.com>
2025-08-18 01:41:56 +0200

    add a comment to explain throttle function

--- src/utils/throttle_util.ts
+++ src/utils/throttle_util.ts
@@ -1,3 +1,12 @@
+// I put this function in a separate file because it can be used across the project.
+// This function receives a callback as an argument. Initially, the callback function will run immediately, then it will set the
+// shouldWait flag to true. After the delay is over, if there are no waiting args, it will be set to false again.
+// If there are waiting args, we execute them and wait for the delay period again.
+// The waiting args come from a call when shouldWait is true, and are always overridden by the last call,
+// because shouldWait and waitingArgs are created once when we declare the function later in the controller, and used
+// as a closure to store and share these two variables.
+
+
 export function throttle<T extends (...args: any[]) => void>(cb: T, delay: number) {
     let shouldWait = false;
     let waitingArgs: Parameters<T> | null;
